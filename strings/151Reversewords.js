72d37be7ec760db10b6e7d5b912bdbda7169494f
bhaviljain <bhaviljain26@gmail.com>
2024-03-01 10:33:32 +0530

    151. Reverse Words in a String

---
+++ strings/151Reversewords.js
@@ -0,0 +1,14 @@
+var reverseWords = function(s) {
+    let ReverseStr ='';
+    let arr = s.split(' ')
+    for(let i = arr.length - 1 ; i>=0; i--)
+    {
+        if(arr[i]==='') continue;
+        if(ReverseStr.length > 0) 
+        ReverseStr+=' '
+        ReverseStr +=arr[i]
+
+    }
+    return ReverseStr;
+};
+console.log(reverseWords("the    sky is blue"));
